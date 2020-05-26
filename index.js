f885d80e666a2708554a7a8b29f137dc31a37518
Abel <enriquezun3@gmail.com>
2020-05-26 14:53:51 -0600

    Agregando los routes y controllers para comenzar a trabajar

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Importar los módulos de express.js
 const express = require('express');
+// Importar todas las rutas disponibles
+const routes = require("./routes");
 
 // Crear un servidor de express
-const app = express()
+const app = express();
+
+// Indicarle a express dónde están las rutas del servidor
+app.use("/", routes());
 
 // Inicializar el servidor en un puerto específico, normalmente se usa 3000 pero puede ser cualquiera
 app.listen(7000, () => {
